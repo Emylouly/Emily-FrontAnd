b241ae80a626a9621cb1c2373178f347ecccd1a5
Emylouly <emilylouisemika@gmail.com>
2025-02-03 14:55:39 -0300

    a

--- src/main/java/br/edu/ifba/demo/frontend/controller/LivroController.java
+++ src/main/java/br/edu/ifba/demo/frontend/controller/LivroController.java
@@ -29,7 +29,7 @@ public class LivroController {
     }
 
     // Buscar livro por ID
-    @GetMapping("/livro/buscarporid")
+    @GetMapping("/livro/buscarporid/{id}")
     public ModelAndView getById(@RequestParam("id") Long id) {
         ModelAndView model = new ModelAndView("livro/form");
         LivroDTO livro = livroService.getById(id);
@@ -43,38 +43,8 @@ public class LivroController {
         return model;
     }
 
-    // Buscar livro pelo ISBN
-    @GetMapping("/livro/buscarporisbn")
-    public ModelAndView getByIsbn(@RequestParam("isbn") String isbn) {
-        ModelAndView model = new ModelAndView();
-        LivroDTO livro = livroService.getByIsbn(isbn);
-
-        if (livro != null) {
-            model.addObject("listaLivro", List.of(livro)); // Atualiza a lista para exibir somente o livro encontrado
-        } else {
-            model.addObject("errorMessage", "Livro não encontrado!");
-        }
-        model.setViewName("livro"); // Reutiliza a mesma view de "livro"
-        return model;
-    }
-
-    // Buscar com titulo
-    @GetMapping("/livro/buscarportitulo")
-    public ModelAndView getByTitulo(@RequestParam("titulo") String titulo) {
-        ModelAndView model = new ModelAndView();
-        LivroDTO livro = livroService.getByTitulo(titulo);
-
-        if (livro != null) {
-            model.addObject("listaLivro", List.of(livro)); // Atualiza a lista para exibir somente o livro encontrado
-        } else {
-            model.addObject("errorMessage", "Livro não encontrado!");
-        }
-        model.setViewName("livro"); // Reutiliza a mesma view de "livro"
-        return model;
-    }
-
     // Deletar livro e redirecionar para a lista de livros
-    @GetMapping("/livros/deletelivro/{id}")
+    @GetMapping("/livro/deletelivro/{id}")
     public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
         livroService.delete(id); 
         redirectAttributes.addFlashAttribute("deletelivro", "Livro excluído com sucesso!");
